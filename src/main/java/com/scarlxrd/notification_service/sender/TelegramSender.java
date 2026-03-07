fd75c97174df0f615810a208720cf7fd41b192a0
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-03-07 14:14:06 -0300

    feat: Telegram sending logic using an interface.

---
+++ src/main/java/com/scarlxrd/notification_service/sender/TelegramSender.java
@@ -0,0 +1,45 @@
+package com.scarlxrd.notification_service.sender;
+
+import com.scarlxrd.notification_service.dto.NotificationPayload;
+import com.scarlxrd.notification_service.impl.NotificationSender;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+
+@Slf4j
+@Component
+public class TelegramSender implements NotificationSender {
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Value("${telegram.bot.chat-id}")
+    private String chatId;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Override
+    public void send(NotificationPayload payload) {
+
+        String message = """
+        📘 Novo cliente registrado
+
+        CPF: %s
+        Nome: %s
+        """.formatted(payload.getCpf(), payload.getName());
+
+        String url = "https://api.telegram.org/bot" + token + "/sendMessage";
+
+        Map<String, Object> body = Map.of(
+                "chat_id", chatId,
+                "text", message
+        );
+
+        restTemplate.postForObject(url, body, String.class);
+
+        log.info("Notificação enviada para Telegram");
+    }
+}
