bfee365b0ac6b62fa45d1af1a0ca361fe2e3047d
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-03-07 15:51:11 -0300

    feat: config to convert JSON with Jackson

---
+++ src/main/java/com/scarlxrd/notification_service/config/rabbitmq/RabbitConfig.java
@@ -0,0 +1,15 @@
+package com.scarlxrd.notification_service.config.rabbitmq;
+
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+}
